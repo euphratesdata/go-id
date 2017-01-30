1525d78284a23cd4aeb6704489361fac664a94fc
Marc Wilson <marcw@scorpioncompute.com>
2017-01-30 04:55:43 +0000

    Update license details.

--- id_test.go
+++ id_test.go
@@ -1,3 +1,7 @@
+// Copyright 2016, 2017 Marc Wilson, Scorpion Compute. All rights
+// reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 package id
 
 import (
